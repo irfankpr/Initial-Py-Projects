209b7d7c08e06f8b837095d134cb63dc555dec43
irfankpr <irfanmuhammedkpr@gmail.com>
2022-08-31 17:29:27 +0530

    added whatpykit

--- msglimit.py
+++ msglimit.py
@@ -1,7 +1,9 @@
 import time
 import pyautogui as p
+import pywhatkit as wp
 
-time.sleep(5)
-for i in range(1000):
-    p.typewrite("nirthada PUBG : "+str(i))
+wp.sendwhatmsg("+91", "Hi", 17, 17)
+
+for i in range(100):
+    p.typewrite("moyanthee")
     p.press("enter")
